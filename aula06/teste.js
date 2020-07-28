adaf4aab2d89bd6b6edb28385a43fc48e36c9e9f
mdsacra <mdsacra@gmail.com>
2020-07-27 21:31:32 -0300

    criando variáveis em JS

---
+++ aula06/teste.js
@@ -0,0 +1,31 @@
+var npos = 5;
+var nneg = -4;
+var nvirg = 5.4;
+var texto = "Textando ou testando? Quem sabe?!";
+var verdade = true;
+
+console.log(npos, nneg, nvirg, nome, verdade);
+
+var nome = "Matheus";
+var sobrenome = "Sacramento";
+
+console.log(nome); //sobrenome Aqui deu erro de sintaxe.
+// console.log(Nome); -- Nome is not defined
+
+console.log(nome, sobrenome);
+
+var textogrande = "Eu vou escrevendo um texto grande, sem saber onde vai dar, mas já que é pra escrever, escreverei bastante. Não tem porque não escrever, com tantas palavras que há no português.Vamos abusar que a palavra é de graça, mas cuidado pra não falar de mais e ficar mal-falado."
+
+console.log(textogrande);
+
+var valorNulo = null;
+var valorUndefined = undefined;
+
+var fruteira = ["mamão", "banana", "maçã", "abacaxi", "limão"]
+
+console.log(fruteira);
+
+var digitalHouse = "Digital House, a escola de programação que forma novas gerações de coders e profissionais digitais para imaginar, inovar e criar o que sempre sonharam."
+
+console.log(digitalHouse);
+
